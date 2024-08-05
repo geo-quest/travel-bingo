327218e27c25e49371c7fcb1e947893ec952f4e4
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-05 23:42:50 +0200

    feat(board-page): some little improvements (wip)

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,54 @@
 /* eslint-disable arrow-parens */
 import "./App.css";
 
-import { Card, List, Table, Typography } from "antd";
+import {
+  FlagTwoTone,
+  PlayCircleOutlined,
+  UnorderedListOutlined,
+} from "@ant-design/icons";
+import { Card, List, Space, Typography } from "antd";
 
 import challenge from "./challenge.json";
 
 const { Title, Paragraph } = Typography;
 
 function App() {
-  const board = challenge.challenges.map(([col1, col2, col3, col4, col5]) => ({
-    col1,
-    col2,
-    col3,
-    col4,
-    col5,
-  }));
-
-  const renderColumn = (challenge: any) => (
-    <a
-      className="challenge-link"
-      onClick={() =>
-        alert(
-          `Challenge: ${challenge.challenge}\nDescription: ${challenge.description}\nPoints: ${challenge.points}`,
-        )
-      }
-    >
-      {challenge.challenge}
-    </a>
-  );
-
-  const columns = ["col1", "col2", "col3", "col4", "col5"].map((title) => ({
-    title,
-    dataIndex: title,
-    key: title,
-    render: renderColumn,
-    ellipsis: true,
-  }));
-
   return (
     <div style={{ padding: "20px" }}>
-      <Card title={challenge.title}>
-        <Paragraph>{challenge.description}</Paragraph>
-        <Title level={4}>Rules</Title>
-        <List
-          bordered
-          dataSource={challenge.rules}
-          renderItem={(rule) => <List.Item>{rule}</List.Item>}
-        />
-        <Title level={4} style={{ marginTop: "20px" }}>
-          Challenges
-        </Title>
-        <Table
-          dataSource={board}
-          columns={columns}
-          showHeader={false}
-          pagination={false}
-          bordered={true}
-        ></Table>
-      </Card>
+      <Space direction="vertical" size={16}>
+        <Card
+          title={
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <FlagTwoTone style={{ marginRight: 8, fontSize: "32px" }} />
+              <h1 style={{ margin: 0 }}>{challenge.title}</h1>
+            </div>
+          }
+        >
+          <Paragraph strong>{challenge.description}</Paragraph>
+          <Title level={4}>
+            <UnorderedListOutlined style={{ marginRight: 8 }} />
+            Rules
+          </Title>
+          <List
+            dataSource={challenge.rules}
+            renderItem={(rule) => <List.Item>{rule}</List.Item>}
+          />
+          <Title level={4} style={{ marginTop: "20px" }}>
+            <PlayCircleOutlined style={{ marginRight: 8 }} />
+            Challenges
+          </Title>
+
+          {challenge.challenges.map((row, rowIndex) => (
+            <div key={rowIndex} style={{ display: "flex" }}>
+              {row.map((item, colIndex) => (
+                <Card.Grid key={colIndex} style={{ width: "20%" }}>
+                  {item.challenge}
+                </Card.Grid>
+              ))}
+            </div>
+          ))}
+        </Card>
+      </Space>
     </div>
   );
 }
