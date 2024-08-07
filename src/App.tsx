2c3cee606a3b96ec6732034ddc0e8c26911ae4d0
rpidanny <abhishekmaharjan1993@gmail.com>
2024-08-07 19:48:57 +0200

    fix: router basename

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,16 @@ import GameEdition from "./pages/GameEdition/GameEdition";
 import Home from "./pages/Home/Home";
 import NoPage from "./pages/NoPage/NoPage";
 
+const getBasename = () => {
+  if (window.location.hostname === "geo-quest.github.io") {
+    return "/travel-bingo";
+  }
+  return "/";
+};
+
 function App() {
-  const basename = process.env.PUBLIC_URL || "/travel-bingo";
   return (
-    <BrowserRouter basename={basename}>
+    <BrowserRouter basename={getBasename()}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/editions/:editionId" element={<GameEdition />} />
