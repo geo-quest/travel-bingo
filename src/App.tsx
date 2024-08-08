0929f8f63d06890b609d9926379ec4806ea7a6e4
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-08 22:55:39 +0200

    feat(leaderboard): initial version

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import gameData from "./data/index";
 import Game from "./pages/Game/Game";
 import Home from "./pages/Home/Home";
+import Run from "./pages/Run/Run";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home data={gameData} />} />
         <Route path="/:gameId" element={<Game data={gameData} />} />
+        <Route path="/:gameId/:runId" element={<Run />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Run/Run.tsx
@@ -0,0 +1,5 @@
+function Run() {
+  return <h1>work in progress...</h1>;
+}
+
+export default Run;
