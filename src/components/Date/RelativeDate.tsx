c6e03ebb65ffb09c2ad5f2fea24af3837f09d553
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-10 21:21:35 +0200

    chore: resolving merge conflicts

--- src/components/Date/RelativeDate.tsx
+++ src/components/Date/RelativeDate.tsx
@@ -1,8 +1,4 @@
 import TimeAgo from "javascript-time-ago";
-<<<<<<< HEAD
-=======
-import en from "javascript-time-ago/locale/en.json";
->>>>>>> main
 import { useTranslation } from "react-i18next";
 
 interface Props {
@@ -11,11 +7,6 @@ interface Props {
 
 const RelativeDate = ({ date }: Props) => {
   const { i18n } = useTranslation();
-<<<<<<< HEAD
-
-=======
-  if (i18n.language.startsWith("en")) TimeAgo.addDefaultLocale(en);
->>>>>>> main
   return (
     <span>{new TimeAgo(i18n.language).format(Date.parse(date || ""))}</span>
   );
