07a746615bcc7081e8b9114e809c29185c819734
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-10 12:22:07 +0200

    chore: refactoring NoPage

--- src/components/NoPage/NoPage.tsx
+++ src/components/NoPage/NoPage.tsx
@@ -1,5 +1,21 @@
+import { FileExclamationTwoTone } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+
 const NoPage = () => {
-  return <h1>404</h1>;
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <FileExclamationTwoTone style={{ fontSize: "72px", marginTop: "20px" }} />
+      <h1 style={{ fontSize: "72px", marginBottom: "20px" }}>Oops!</h1>
+      <h2>Something went wrong.</h2>
+      <p>The page you're looking for doesn't exist.</p>
+      <Link
+        to="/"
+        style={{ textDecoration: "none", color: "#007BFF", fontSize: "18px" }}
+      >
+        Go back to the homepage
+      </Link>
+    </div>
+  );
 };
 
 export default NoPage;
