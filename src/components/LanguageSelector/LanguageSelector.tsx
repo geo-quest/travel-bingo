be35bb6590d3cd662bbf31efda9c8dcd654091df
rpidanny <abhishekmaharjan1993@gmail.com>
2024-08-17 09:28:51 +0200

    feat: add romanian and french languages

--- src/components/LanguageSelector/LanguageSelector.tsx
+++ src/components/LanguageSelector/LanguageSelector.tsx
@@ -14,6 +14,8 @@ const LanguageSelector = () => {
       <Option value="en">English</Option>
       <Option value="pt">Português</Option>
       <Option value="nl">Nederlands</Option>
+      <Option value="ro">Română</Option>
+      <Option value="fr">Français</Option>
       <Option value="zh">中文</Option>
     </Select>
   )
--- src/i18n.ts
+++ src/i18n.ts
@@ -3,8 +3,10 @@ import LanguageDetector from 'i18next-browser-languagedetector'
 import Backend from 'i18next-http-backend'
 import TimeAgo from 'javascript-time-ago'
 import en from 'javascript-time-ago/locale/en.json'
+import fr from 'javascript-time-ago/locale/fr.json'
 import nl from 'javascript-time-ago/locale/nl.json'
 import pt from 'javascript-time-ago/locale/pt.json'
+import ro from 'javascript-time-ago/locale/ro.json'
 import zh from 'javascript-time-ago/locale/zh.json'
 import { initReactI18next } from 'react-i18next'
 
@@ -16,7 +18,7 @@ i18n
     backend: {
       loadPath: '/locales/{{lng}}.json',
     },
-    supportedLngs: ['en', 'nl', 'pt', 'zh'],
+    supportedLngs: ['en', 'nl', 'pt', 'zh', 'ro', 'fr'],
     fallbackLng: 'en',
     debug: false,
     interpolation: {
@@ -32,5 +34,7 @@ TimeAgo.addDefaultLocale(en)
 TimeAgo.addLocale(nl)
 TimeAgo.addLocale(pt)
 TimeAgo.addLocale(zh)
+TimeAgo.addLocale(ro)
+TimeAgo.addLocale(fr)
 
 export default i18n
